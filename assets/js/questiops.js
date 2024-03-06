1d6ae06c8166587c285716b54351cc3b85880565
Neverton Sousa <nevertonsousa2@gmail.com>
2024-03-05 23:25:39 -0300

    questions

---
+++ assets/js/questiops.js
@@ -0,0 +1,42 @@
+let question = [
+    {
+        numb: 1,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    }
+]
